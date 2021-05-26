f48570d39e7ee3b8a59c7a2acc3f8b14e7670d58
Luke Golden <luke@whiteboard.is>
2021-05-26 10:25:37 -0400

    Added Empty Collection Hide Script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # webflow-scripts
+
+##Usage
+###This is a repository to save useful scripts for Webflow projects.
+
+Note, all Javascript should be wrapped in `<script>` tags. 
---
+++ javascript/hideEmptyCollectionList.js
@@ -0,0 +1,4 @@
+//This is a a lovely script to hide a section if the collection list inside of it is empty:
+
+$('.w-dyn-empty').parents('.section').each(function(){ $(this).hide()});
+
